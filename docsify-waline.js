5475953107930a1a7119b55104483f76d6150ce4
cxcn <cxprcn@gmail.com>
2021-07-30 02:39:21 +0800

    fix bug

--- docsify-waline.js
+++ docsify-waline.js
@@ -1,29 +1,28 @@
 function install(hook, vm) {
+  var options = vm.config.waline || {};
+  if (!options.serverURL) {
+    console.warn('sorry, waline.serverURL must be required.');
+    return;
+  }
   const dom = Docsify.dom;
-
-  hook.ready((_) => {
-    if (!window.Waline) {
-      console.warn('please include script before init $docsify');
-    }
-  });
+  var w = false;
 
   hook.mounted((_) => {
     const div = dom.create('div');
     div.id = 'waline';
     const main = dom.getNode('#main');
-    // div.style = "width: " + i.clientWidth + "px !important; margin: 0 auto 20px;", a.appendTo(a.find(".content"), n);
-    div.style = 'max-width: 80% !important; margin: 0 auto 20px;';
-    dom.appendTo(dom.find('.content'), div); // responsive design
+    div.style = `width: ${main.clientWidth}px; margin: 0 auto 20px;`;
+    dom.appendTo(dom.find('.content'), div);
+    options.el = document.getElementById('waline');
   });
 
   hook.doneEach((_) => {
-    var options = vm.config.waline || {};
-    if (!options.serverURL) {
-      console.warn('sorry, serverURL must be required.');
-      return;
+    if (w) {
+      w.destroy();
     }
-    options.el = document.getElementById('waline');
-    Waline(options);
+    options.path = location.hash;
+    w = Waline(options);
+    // console.log(options);
   });
 }
 
